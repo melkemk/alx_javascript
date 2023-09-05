3be1f34f53969d067c065533803d30f839501855
melkamu <melkemk503@gmail.com>
2023-09-05 14:31:45 +0300

    2.rec

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+
+      return {};
+    }
     this.width = w;
     this.height = h;
-    if(w<=0 || h<=0){
-    	return {};
-    }
   }
 }
 
+
+const r4 = new Rectangle(2, 0);
+console.log(r4);
+console.log(r4.width);
+console.log(r4.height);
 
 module.exports = Rectangle;
